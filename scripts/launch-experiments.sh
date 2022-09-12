606f93d215519057a86a407fa14e7257e95f8b9b
Dominic Thorn <dominic.thorn@gmail.com>
2022-09-12 13:10:31 +0000

    WIP

--- scripts/launch-experiments.sh
+++ scripts/launch-experiments.sh
@@ -1,7 +1,7 @@
 #python src/mscproject/experiment.py -m ALL
-#python src/mscproject/experiment.py -m GCN
+python src/mscproject/experiment.py -m GCN
 python src/mscproject/experiment.py -m GraphSAGE
-#python src/mscproject/experiment.py -m GAT
-#python src/mscproject/experiment.py -m HGT
-#python src/mscproject/experiment.py -m HAN
+python src/mscproject/experiment.py -m GAT
+python src/mscproject/experiment.py -m HGT
+python src/mscproject/experiment.py -m HAN
 exit 0 
