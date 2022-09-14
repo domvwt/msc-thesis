d48f14c38ba7d8ad94b72bbea0f0c276a999ba84
Dominic Thorn <dominic.thorn@gmail.com>
2022-09-14 11:16:19 +0100

    Separate simulation from feat generation

---
+++ src/mscproject/jobs/05_make_anomalies.py
@@ -0,0 +1,64 @@
+"""
+- Simulate anomalous nodes / edges
+- Create graph features
+"""
+
+from pathlib import Path
+
+import pandas as pd
+import yaml
+
+import mscproject.dataprep as dp
+import mscproject.features as feat
+import mscproject.simulate as sim
+
+
+def main() -> None:
+    # Read config.
+    conf_dict = yaml.safe_load(Path("config/conf.yaml").read_text())
+
+    # Load graph data.
+    print("Loading graph data...")
+    companies_nodes_df = pd.read_parquet(conf_dict["companies_nodes"])
+    persons_nodes_df = pd.read_parquet(conf_dict["persons_nodes"])
+    edges_df = pd.read_parquet(conf_dict["edges"])
+
+    # Sample records if sample size is specified.
+    if conf_dict["sample_size"] is not None:
+        print(f"Sampling {conf_dict['sample_size']} records...")
+        companies_nodes_df = companies_nodes_df.sample(conf_dict["sample_size"])
+        persons_nodes_df = persons_nodes_df.sample(conf_dict["sample_size"])
+        edges_df = edges_df.sample(conf_dict["sample_size"])
+
+    # Simulate anomalous nodes and edges.
+    print("Simulating anomalous nodes and edges...")
+    companies_anomalised, persons_anomalised, edges_anomalised = sim.simulate_anomalies(
+        companies_nodes_df, persons_nodes_df, edges_df
+    )
+
+    # Get new component ids.
+    print("Updating component ids...")
+    new_graph = feat.make_graph(edges_anomalised)
+    node_id_to_cc_id_map = feat.get_node_id_to_cc_id_map(new_graph)
+    companies_anomalised[
+        "component"
+    ] = companies_anomalised["id"].map(node_id_to_cc_id_map)
+    persons_anomalised["component"] = persons_anomalised[
+        "id"
+    ].map(node_id_to_cc_id_map)
+    edges_df["component"] = edges_df["src"].map(node_id_to_cc_id_map)
+
+    # Save features.
+    print("Saving anomalies...")
+    # ? Swapped dict for list of tuples as pd.DataFrame is unhashable.
+    output_path_map = [
+        (companies_anomalised, conf_dict["companies_anomalies"]),
+        (persons_anomalised, conf_dict["persons_anomalies"]),
+        (edges_anomalised, conf_dict["edges_anomalies"]),
+    ]
+    dp.write_output_path_map(output_path_map)
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
--- src/mscproject/jobs/06_make_graph_features.py
+++ src/mscproject/jobs/06_make_graph_features.py
@@ -10,7 +10,6 @@ import yaml
 
 import mscproject.dataprep as dp
 import mscproject.features as feat
-import mscproject.simulate as sim
 
 
 def main() -> None:
@@ -18,27 +17,21 @@ def main() -> None:
     conf_dict = yaml.safe_load(Path("config/conf.yaml").read_text())
 
     # Load graph data.
-    print("Loading graph data...")
-    companies_nodes_df = pd.read_parquet(conf_dict["companies_nodes"])
-    persons_nodes_df = pd.read_parquet(conf_dict["persons_nodes"])
-    edges_df = pd.read_parquet(conf_dict["edges"])
+    print("Loading anomalised data...")
+    companies_df = pd.read_parquet(conf_dict["companies_anomalies"])
+    persons_df = pd.read_parquet(conf_dict["persons_anomalies"])
+    edges_df = pd.read_parquet(conf_dict["edges_anomalies"])
 
     # Sample records if sample size is specified.
     if conf_dict["sample_size"] is not None:
         print(f"Sampling {conf_dict['sample_size']} records...")
-        companies_nodes_df = companies_nodes_df.sample(conf_dict["sample_size"])
-        persons_nodes_df = persons_nodes_df.sample(conf_dict["sample_size"])
+        companies_df = companies_df.sample(conf_dict["sample_size"])
+        persons_df = persons_df.sample(conf_dict["sample_size"])
         edges_df = edges_df.sample(conf_dict["sample_size"])
-
-    # Simulate anomalous nodes and edges.
-    print("Simulating anomalous nodes and edges...")
-    companies_anomalised, persons_anomalised, edges_anomalised = sim.simulate_anomalies(
-        companies_nodes_df, persons_nodes_df, edges_df
-    )
 
     # Generate graph features.
     print("Generating graph features...")
-    graph_nx = feat.make_graph(edges_anomalised)
+    graph_nx = feat.make_graph(edges_df)
     node_features_df = feat.generate_node_features(graph_nx)
     # neighbourhood_features_df = feat.get_local_neighbourhood_features_fast(
     #     graph_nx, node_features_df, 2
@@ -57,7 +50,7 @@ def main() -> None:
     # Join features to nodes.
     print("Joining features to nodes...")
     companies_nodes_with_features_df01 = pd.merge(
-        companies_anomalised, node_features_df, how="left", on="id"
+        companies_df, node_features_df, how="left", on="id"
     )
     companies_nodes_with_features_df02 = pd.merge(
         companies_nodes_with_features_df01,
@@ -66,7 +59,7 @@ def main() -> None:
         on="id",
     )
     persons_nodes_with_features_df01 = pd.merge(
-        persons_anomalised, node_features_df, how="left", on="id"
+        persons_df, node_features_df, how="left", on="id"
     )
     persons_nodes_with_features_df02 = pd.merge(
         persons_nodes_with_features_df01,
@@ -75,25 +68,12 @@ def main() -> None:
         on="id",
     )
 
-    # Get new component ids.
-    print("Updating component ids...")
-    new_graph = feat.make_graph(edges_anomalised)
-    node_id_to_cc_id_map = feat.get_node_id_to_cc_id_map(new_graph)
-    companies_nodes_with_features_df02[
-        "component"
-    ] = companies_nodes_with_features_df02["id"].map(node_id_to_cc_id_map)
-    persons_nodes_with_features_df02["component"] = persons_nodes_with_features_df02[
-        "id"
-    ].map(node_id_to_cc_id_map)
-    edges_anomalised["component"] = edges_anomalised["src"].map(node_id_to_cc_id_map)
-
     # Save features.
     print("Saving features...")
-    # ? Swapped dict for list of tuples as pd.DataFrame is unhashable.
     output_path_map = [
         (companies_nodes_with_features_df02, conf_dict["companies_features"]),
         (persons_nodes_with_features_df02, conf_dict["persons_features"]),
-        (edges_anomalised, conf_dict["edges_features"]),
+        (edges_df, conf_dict["edges_features"]),
     ]
     dp.write_output_path_map(output_path_map)
     print("Done.")
