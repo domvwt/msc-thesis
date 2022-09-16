61b0f587c0e8c64371812ed38d061e908246e3e9
Dominic Thorn <dominic.thorn@gmail.com>
2022-09-16 22:46:52 +0000

    WIP

--- scripts/launch-experiments.sh
+++ scripts/launch-experiments.sh
@@ -11,11 +11,11 @@ OPTUNA_DB="sqlite:///optuna-02.db"
 
 #python src/mscproject/experiment.py -m ALL
 
-python src/mscproject/experiment.py -m GCN -db $OPTUNA_DB
-python src/mscproject/experiment.py -m GraphSAGE -db $OPTUNA_DB
-python src/mscproject/experiment.py -m GAT -db $OPTUNA_DB
-python src/mscproject/experiment.py -m HGT -db $OPTUNA_DB
-python src/mscproject/experiment.py -m HAN -db $OPTUNA_DB
+python src/mscproject/experiment.py -m GCN --db $OPTUNA_DB
+python src/mscproject/experiment.py -m GraphSAGE --db $OPTUNA_DB
+python src/mscproject/experiment.py -m GAT --db $OPTUNA_DB
+python src/mscproject/experiment.py -m HGT --db $OPTUNA_DB
+python src/mscproject/experiment.py -m HAN --db $OPTUNA_DB
 
 # export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:128
 
