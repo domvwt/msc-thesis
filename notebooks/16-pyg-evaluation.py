2f97307bf0f649ba079c666e5b0a0ddee4338bb5
Dominic Thorn <dominic.thorn@gmail.com>
2022-09-16 19:53:53 +0100

    Eval work

---
+++ notebooks/16-pyg-evaluation.py
@@ -0,0 +1,259 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.1
+#   kernelspec:
+#     display_name: 'Python 3.9.12 (''.venv'': poetry)'
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import os
+from pathlib import Path
+
+import numpy as np
+import optuna
+import pandas as pd
+import torch
+from tqdm import tqdm
+from torch_geometric.transforms import AddSelfLoops
+from mscproject.transforms import RemoveSelfLoops
+
+from mscproject.datasets import CompanyBeneficialOwners
+from mscproject.transforms import RemoveSelfLoops
+import mscproject.models as mod
+import mscproject.experiment as exp
+
+while not Path("data") in Path(".").iterdir():
+    os.chdir("..")
+
+# %%
+study_names = (
+    # "pyg_model_selection_ALL",
+    "pyg_model_selection_GCN",
+    "pyg_model_selection_GraphSAGE",
+    "pyg_model_selection_GAT",
+    "pyg_model_selection_HGT",
+    "pyg_model_selection_HAN",
+)
+
+# %%
+from IPython.display import display
+
+# %%
+OPTUNA_STORAGE = "sqlite:///data/optuna.db"
+MODEL_DIR = Path("data/models/pyg")
+PREDICTION_DIR = Path("data/predictions")
+
+Path(MODEL_DIR).mkdir(parents=True, exist_ok=True)
+Path(PREDICTION_DIR).mkdir(parents=True, exist_ok=True)
+
+trials_dfs = [
+    study.trials_dataframe().assign(study_name=study.study_name)
+    for study in (
+        optuna.load_study(study_name=study_name, storage=OPTUNA_STORAGE)
+        for study_name in study_names
+    )
+]
+eval_df = pd.concat(trials_dfs, join="inner", axis=0)
+
+
+# %%
+# for study in (
+#     optuna.load_study(study_name=study_name, storage=OPTUNA_STORAGE)
+#     for study_name in study_names
+# ):
+#     print(study.study_name)
+#     # Plot the results.
+#     mean_top_10_loss = study.trials_dataframe()["value"].sort_values().head(10).mean()
+#     print("Mean of top 10 best loss:", mean_top_10_loss)
+#     optuna.visualization.plot_optimization_history(study).show()
+#     # optuna.visualization.plot_contour(study).show()
+#     optuna.visualization.plot_slice(study).show()
+#     optuna.visualization.plot_param_importances(study).show()
+#     print()
+#     print()
+
+# %%
+def mark_outliers(df):
+    df = df.copy()
+    upper_threshold = df.value.mean() + 3 * df.value.std()
+    df["outlier"] = df.value > upper_threshold
+    return df
+
+
+# %%
+metric = "user_attrs_aprc"
+first = True
+
+best_trials = {}
+
+for study_name, df in zip(study_names, trials_dfs):
+    if study_name.endswith("_ALL"):
+        continue
+    print(study_name)
+    top = df.sort_values(metric, ascending=False)[:30]
+    param_columns = [x for x in top.columns if x.startswith("params")]
+    top = mark_outliers(top)
+    display(top[["outlier", "value", metric, *param_columns]][:10])
+
+    model_type = study_name.split("_")[-1]
+    best_trials[model_type] = top.query("not outlier").iloc[0].to_dict()
+    best_trials[model_type]["model_type"] = model_type
+
+    print()
+
+# %%
+import mscproject.experiment as exp
+
+
+# %%
+# remove prefix from string
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix) :]
+    return text
+
+
+# %%
+def build_experiment_from_trial_params(trial_params, dataset, verbose=False):
+    param_dict = {
+        remove_prefix(k, "params_"): v
+        for k, v in trial_params.items()
+        if k.startswith("params")
+    }
+    # Rename key from "n_layers" to "num_layers"
+    if "n_layers" in param_dict:
+        param_dict["num_layers"] = param_dict.pop("n_layers")
+    param_dict["in_channels"] = -1
+    param_dict["out_channels"] = 1
+    param_dict["act_first"] = True
+    param_dict["add_self_loops"] = False
+    param_dict["model_type"] = mod.get_model(trial_params["model_type"])
+    param_dict["v2"] = True
+    lr = trial_params["user_attrs_learning_rate"]
+    param_dict["jk"] = None if param_dict["jk"] == "none" else param_dict["jk"]
+    if verbose:
+        print(param_dict)
+    return exp.get_model_and_optimiser(param_dict, dataset, lr)
+
+
+# %%
+dataset_path = "data/pyg/"
+
+# Set the device.
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {str(device).upper()}")
+
+# Load the dataset.
+dataset = CompanyBeneficialOwners(dataset_path, to_undirected=True)
+dataset = dataset.data.to(device)
+
+model_metrics = {}
+
+models_dir = Path("models/pyg")
+models_dir.mkdir(parents=True, exist_ok=True)
+
+for model_name in best_trials.keys():
+
+    model_path = MODEL_DIR / f"{model_name}.pt"
+    if model_path.exists():
+        print(f"Model {model_name} already exists, skipping")
+        continue
+
+    print("Training model:", model_name)
+    trial_dict = best_trials[model_name]
+
+    if trial_dict["params_add_self_loops"]:
+        dataset = AddSelfLoops(fill_value=1.0)(dataset)  # type: ignore
+    else:
+        dataset = RemoveSelfLoops()(dataset)
+
+    model, optimiser = build_experiment_from_trial_params(
+        trial_dict, dataset, verbose=True
+    )
+
+    # Train and evaluate the model.
+    best_epoch = int(trial_dict["user_attrs_best_epoch"])
+
+    progress = tqdm(range(best_epoch))
+
+    for epoch in progress:
+        loss = exp.train(model, dataset, optimiser, on_val=True)
+        progress.set_description(f"Train loss: {loss:.4f}")
+
+    eval_metrics = exp.evaluate(
+        model, dataset, on_train=False, on_val=False, on_test=True
+    )
+
+    model_metrics[model_name] = eval_metrics.test
+    print(eval_metrics.test)
+
+    # Save the trained model.
+    torch.save(model.state_dict(), model_path)
+    print()
+
+# %%
+# Load and evaluate models
+for model_name in best_trials.keys():
+
+    model_path = MODEL_DIR / f"{model_name}.pt"
+    if not model_path.exists():
+        print(f"Model {model_name} does not exist, skipping")
+        continue
+
+    trial_params = best_trials[model_name]
+
+    dataset = CompanyBeneficialOwners(dataset_path, to_undirected=True)
+    dataset = dataset.data.to(device)
+
+    model, _ = build_experiment_from_trial_params(trial_params, dataset)
+    model.load_state_dict(torch.load(model_path, map_location=torch.device(device)))
+
+    model.to(device)
+
+    if trial_params["params_add_self_loops"]:
+        dataset = AddSelfLoops(fill_value=1.0)(dataset)
+    else:
+        dataset = RemoveSelfLoops()(dataset)
+
+    eval_metrics = exp.evaluate(
+        model, dataset, on_train=False, on_val=False, on_test=True
+    )
+
+    model_metrics[model_name] = eval_metrics.test
+    print(model_name, eval_metrics.test)
+
+    prediction_dict = model(dataset.x_dict, dataset.edge_index_dict)
+    prediction_df_list = []
+
+    for node_type in dataset.node_types:
+        prediction = (
+            prediction_dict[node_type][dataset[node_type].test_mask]
+            .detach()
+            .numpy()
+            .flatten()
+        )
+        actual = (
+            dataset.y_dict[node_type][dataset[node_type].test_mask]
+            .detach()
+            .numpy()
+            .flatten()
+        )
+        df = pd.DataFrame({"pred_proba": prediction, "actual": actual})
+        prediction_df_list.append(df)
+
+    prediction_df = pd.concat(prediction_df_list)
+    prediction_df.to_csv(PREDICTION_DIR / f"{model_name}.csv", index=False)
+
+# %%
+performance_comparison = pd.DataFrame.from_dict(model_metrics, orient="index")
+performance_comparison.to_csv("reports/test-performance-pyg.csv", index_label="model")
+
+# %%
---
+++ notebooks/18-graph-visualisation.py
@@ -0,0 +1,295 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.1
+#   kernelspec:
+#     display_name: 'Python 3.9.12 (''.venv'': poetry)'
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import os
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import torch
+import sklearn
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import networkx as nx
+import yaml
+
+import mscproject.models as mod
+import mscproject.experiment as exp
+
+while not Path("data") in Path(".").iterdir():
+    os.chdir("..")
+
+# %%
+# Matplotlib Settings
+plt.style.use("seaborn-whitegrid")
+plt.style.use("seaborn-paper")
+
+FONT_SIZE = 12
+
+# Set plot font size
+plt.rcParams.update({"font.size": 8})
+
+# Set axis label font size
+plt.rcParams.update({"axes.labelsize": 10})
+
+# Set legend font size
+plt.rcParams.update({"legend.fontsize": 10})
+
+# Set tick label font size
+plt.rcParams.update({"xtick.labelsize": 9})
+
+# Set tick label font size
+plt.rcParams.update({"ytick.labelsize": 9})
+
+# Set figure title font size
+plt.rcParams.update({"axes.titlesize": FONT_SIZE})
+
+# %%
+conf_dict = yaml.safe_load(Path("config/conf.yaml").read_text())
+
+# %%
+edges_pre_df = pd.read_parquet(conf_dict["edges"])
+
+# %%
+edges_post_df = pd.read_parquet(conf_dict["edges_anomalies"])
+
+# %%
+
+# %% [markdown]
+# ## Graph Statistics
+
+# %%
+edges_post_df.is_anomalous.value_counts(normalize=True)
+
+# %%
+# Get component sizes
+component_sizes_pre = edges_pre_df.groupby("component").agg({
+    "src": "count"
+})
+# Select 99% of components
+component_sizes_pre = component_sizes_pre.sort_values("src", ascending=True).query("src > 9")
+component_sizes_pre = component_sizes_pre.iloc[:int(len(component_sizes_pre) * 0.99)]
+component_sizes_pre["src"].plot.hist(bins=20, label="Normal")
+
+# %%
+graph_post = nx.from_pandas_edgelist(edges_post_df, "src", "dst")
+
+# %%
+# # Get connected components
+# components_post = list(nx.connected_components(graph_post))
+# # Assign components to edges
+# edges_post_df["component"] = edges_post_df["src"].apply(
+#     lambda x: [i for i, c in enumerate(components_post) if x in c][0]
+# )
+
+# %%
+# Get component sizes
+component_sizes_post = edges_post_df.groupby("component").agg({
+    "is_anomalous": "sum",
+    "src": "count"
+})
+# Select 99% of components
+component_sizes_post = component_sizes_post.sort_values("src", ascending=True)
+component_sizes_post = component_sizes_post.iloc[:int(len(component_sizes_post) * 0.99)]
+component_sizes_post["src"].plot.hist(bins=20, label="Normal")
+
+# %%
+
+# %%
+
+# %%
+# Get component sizes
+component_sizes_post = edges_post_df.groupby(["component"]).agg({
+    "is_anomalous": "sum",
+    "src": "count"
+})
+component_sizes_post["has_anomaly"] = component_sizes_post["is_anomalous"] > 0
+# Select 99% of components
+component_sizes_post = component_sizes_post.sort_values("src", ascending=True)
+component_sizes_post = component_sizes_post.iloc[:int(len(component_sizes_post) * 0.99)]
+anomaly_dist = component_sizes_post.groupby(["src", "has_anomaly"]).count().unstack()
+# normalise
+anomaly_dist = anomaly_dist / anomaly_dist.sum()
+anomaly_dist.plot.hist(bins=20, alpha=0.5, label="Normal")
+
+# %%
+component_sizes_post
+
+# %%
+component_sizes_pre.groupby("src").size().sort_index()[:20]
+
+# %%
+component_sizes_post.groupby("src").size().sort_index()[:20]
+
+# %%
+component_sizes_post.groupby("src").agg(["sum", "count"])
+
+# %% [markdown]
+# ## Anomaly Simulation
+
+# %%
+graph_edges_post = edges_post_df.query("component == 23")
+graph_edges_post
+
+# %%
+edges_pre_df.query("src == '2258222399048453312'")
+
+# %%
+graph_edges_pre = edges_pre_df.query("component == 77309426934").sort_values("src")
+
+# %%
+edges_in_pre_not_in_post = graph_edges_pre.query("src not in @graph_edges_post.src.values")
+edges_in_pre_not_in_post
+
+# %%
+graph_pre = nx.DiGraph()
+graph_pre.add_nodes_from(graph_edges_pre["src"].unique())
+graph_pre.add_nodes_from(graph_edges_pre["dst"].unique())
+graph_pre.add_edges_from(graph_edges_pre[["src", "dst"]].values)
+
+# %%
+graph_post = nx.DiGraph()
+graph_post.add_nodes_from(graph_edges_post["src"].unique())
+graph_post.add_nodes_from(graph_edges_post["dst"].unique())
+graph_post.add_edges_from(graph_edges_post[["src", "dst"]].values)
+
+# %%
+node_c = "lightblue"
+new_node_c = "lightgreen"
+dropped_node_c = "lightpink"
+anomalous_node_c = "darkorange"
+
+edge_c = "lightgrey"
+new_edge_c = "green"
+dropped_edge_c = "red"
+
+# new_edge_c = edge_c
+# dropped_edge_c = edge_c
+
+
+# %%
+graph_pre.nodes
+
+# %%
+i = 168
+print(i)
+pos = nx.drawing.spring_layout(graph_pre, seed=i, iterations=100, k=0.8)
+
+NODE_SIZE = 100
+
+# %%
+anomaly_id = "2258222399048453312"
+pos[anomaly_id] = [-0.40 ,  0.21]
+
+
+# %%
+def plot_pre(ax):
+
+    edge_colours = [
+        edge_c for u, v in graph_pre.edges
+    ]
+    node_colours = [
+        node_c for n in graph_pre.nodes
+    ]
+
+    # pos = nx.drawing.spring_layout(graph_intermediate, iterations=1000, seed=i)
+    nx.draw(
+        graph_pre,
+        pos=pos,
+        edge_color=edge_colours,
+        node_color=node_colours,
+        # labels={n: n[:3] for n in graph_pre.nodes},
+        node_size=NODE_SIZE,
+        # font_size=8,
+        # font_color="black",
+        ax=ax
+    )
+
+
+# %%
+def plot_mid(ax):
+    # Colour edges that are in the pre graph but not in the post graph
+    edge_colours = [
+        dropped_edge_c if (u) == "2258222399048453312" else edge_c for u, v in graph_pre.edges
+    ]
+    node_colours = [
+        node_c if n in graph_post.nodes else dropped_node_c for n in graph_pre.nodes
+    ]
+
+    # Set colour for node 225
+    idx = next(i for i, j in enumerate(graph_pre.nodes) if j == "2258222399048453312")
+    node_colours[idx] = anomalous_node_c
+
+    # pos = nx.drawing.spring_layout(graph_intermediate, iterations=1000, seed=i)
+    nx.draw(
+        graph_pre,
+        pos=pos,
+        edge_color=edge_colours,
+        node_color=node_colours,
+        # labels={n: n[:3] for n in graph_pre.nodes},
+        node_size=NODE_SIZE,
+        # font_size=8,
+        # font_color="black",
+        ax=ax
+    )
+
+
+# %%
+def plot_post(ax):
+    pos2 = {**pos, "11395388473527546194": pos["17957350210417364187"]}
+
+    # Colour edges in the post graph that are not in the pre graph
+    edge_colours = [
+        new_edge_c if u == "2258222399048453312" else edge_c for u, v in graph_post.edges
+    ]
+    node_colours = [
+        node_c if n in graph_pre.nodes else new_node_c for n in graph_post.nodes
+    ]
+
+    # Set colour for node 225
+    idx = next(i for i, j in enumerate(graph_post.nodes) if j == "2258222399048453312")
+    node_colours[idx] = anomalous_node_c
+
+    # pos = nx.drawing.spring_layout(graph_intermediate, iterations=1000, seed=i)
+    nx.draw(
+        graph_post,
+        pos=pos2,
+        edge_color=edge_colours,
+        node_color=node_colours,
+        # labels={n: n[:3] for n in graph_post.nodes},
+        node_size=NODE_SIZE,
+        # font_size=8,
+        # font_color="black",
+        ax=ax
+    )
+
+
+# %%
+fig, axes = plt.subplots(3, 1, figsize=(4, 12))
+axes[0].set_title("Pre-Shuffle")
+axes[1].set_title("Anomaly Assignment")
+axes[2].set_title("Post-Shuffle")
+
+plot_pre(axes[0])
+plot_mid(axes[1])
+plot_post(axes[2])
+
+fig.savefig("figures/anomaly-simulation.png", dpi=300, bbox_inches="tight")
+
+
+# %%
+
+# %%
