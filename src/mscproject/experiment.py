6450e8c68490d42fd93d1613958df23bc48aab87
Dominic Thorn <dominic.thorn@gmail.com>
2022-09-17 12:52:27 +0100

    WIP

--- src/mscproject/experiment.py
+++ src/mscproject/experiment.py
@@ -517,9 +517,13 @@ def optimise_design(trial: optuna.Trial, dataset: HeteroData, model_type_name: s
         early_stopping(eval_metrics.val.average_precision)
         time_per_epoch = (time.time() - start_time) / (early_stopping.epoch + 1)
         print(
-            f"Epoch: {early_stopping.epoch};"
-            f"Time per epoch: {time_per_epoch:.2f}s;"
-            f"Val APRC: {eval_metrics.val.average_precision:.4f}",
+            ";".join(
+                [
+                    f"Epoch: {early_stopping.epoch}"
+                    f"Time per epoch: {time_per_epoch:.2f}s"
+                    f"Val APRC: {eval_metrics.val.average_precision:.4f}"
+                ]
+            ),
             flush=True,
             end="\r",
         )
