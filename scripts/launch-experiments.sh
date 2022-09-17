d6762a5ac20e28972efa9da7ba4d336324d6aa66
Dominic Thorn <dominic.thorn@gmail.com>
2022-09-17 18:06:05 +0000

    WIP

--- scripts/launch-experiments.sh
+++ scripts/launch-experiments.sh
@@ -28,18 +28,18 @@ python src/mscproject/experiment.py \
 
 
 EXPERIMENT_TYPE="HYPERPARAMETERS"
-TARGET_TRIALS=30
-
-python src/mscproject/experiment.py \
-  -m GraphSAGE \
-  --db $OPTUNA_DB \
-  -n $TARGET_TRIALS \
-  -e $EXPERIMENT_TYPE
+TARGET_TRIALS=20
 
 python src/mscproject/experiment.py \
   -m GCN \
   --db $OPTUNA_DB \
   -n $TARGET_TRIALS \
-  -e $EXPERIMENT_TYPE
+  -e $EXPERIMENT_TYPE \
+
+python src/mscproject/experiment.py \
+  -m GraphSAGE \
+  --db $OPTUNA_DB \
+  -n $TARGET_TRIALS \
+  -e $EXPERIMENT_TYPE \
 
 exit 0 
