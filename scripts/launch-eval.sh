d246c8aab628a4d2e71c23a0980d5c8e53e31159
Dominic Thorn <dominic.thorn@gmail.com>
2022-09-19 23:37:55 +0100

    WIP

--- scripts/launch-eval.sh
+++ scripts/launch-eval.sh
@@ -1,5 +1,5 @@
 pip install --user . --no-dependencies
 #export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:128
 #export CUDA_VISIBLE_DEVICES=""
-python notebooks/16-pyg-evaluation.py
+python notebooks/16-gnn-evaluation.py
 exit 0 
